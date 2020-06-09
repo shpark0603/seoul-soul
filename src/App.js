e2a566b4d6256928fcd3bdbdf160e247ccfd360a
shpReacts <shp.react@gmail.com>
2020-06-10 04:40:50 +0900

    Create sign up component and store authenticated user into firebase firestore

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from "./pages/home/home.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./components/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import "./App.css";
 
 class App extends Component {
@@ -15,8 +15,21 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapshot) => {
+          const userData = {
+            id: snapshot.id,
+            ...snapshot.data(),
+          };
+
+          this.setState({ currentUser: userData });
+        });
+      } else {
+        this.setState({ currentUser: null });
+      }
     });
   }
 
